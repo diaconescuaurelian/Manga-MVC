9f93e63f369e5f614c74364b841099481a628597
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-04-25 18:49:58 +0300

    Create Category model, add Entity Frameork Core andn setup ApplicationDbContext

---
+++ MangaWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MangaWebApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
