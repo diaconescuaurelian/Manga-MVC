0dd56b83d109fc28a6d4f91b894403f13e181681
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-04-26 17:07:27 +0300

    Add CategoryController, Add Category link in the header and seed Category table

--- MangaWebApp/Data/ApplicationDbContext.cs
+++ MangaWebApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MangaWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MangaWebApp.Data
 {
@@ -7,6 +8,17 @@ namespace MangaWebApp.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Shonen", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Shojo", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Seinin", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Josei", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Kodomomuke", DisplayOrder = 5 }
+                );
         }
     }
 }
