bfd614ca22f7aaf8375a3d93c05f6a5f31cdd0fd
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-04-27 21:59:27 +0300

    Made a RezorPages version of the project

---
+++ MangaWebAppRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MangaWebAppRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangaWebAppRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Shonen", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Shojo", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Seinin", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Josei", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Kodomomuke", DisplayOrder = 5 }
+                );
+        }
+    }
+}
