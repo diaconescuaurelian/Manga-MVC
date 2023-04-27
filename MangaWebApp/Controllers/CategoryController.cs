03c225d5bef72bf2e786a389f0330e8b8982f42c
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-04-27 04:42:30 +0300

    Add validations

--- MangaWebApp/Controllers/CategoryController.cs
+++ MangaWebApp/Controllers/CategoryController.cs
@@ -24,9 +24,21 @@ namespace MangaWebApp.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            //if (obj.Name != null && obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Test is an invalid value");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
