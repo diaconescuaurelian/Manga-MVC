5e2a41cf1a1b3d173ae737b7a8e33a13007525d6
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-04-27 16:19:49 +0300

    Add Edit page

--- MangaWebApp/Controllers/CategoryController.cs
+++ MangaWebApp/Controllers/CategoryController.cs
@@ -40,5 +40,34 @@ namespace MangaWebApp.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
