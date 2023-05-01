812e9d88b07832d1a42ca73de22d0a6ce4aec7ba
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-05-01 15:45:28 +0300

    Add Category droplist in Create Product page with ViewData

--- MangaWebApp/Areas/Admin/Controllers/ProductController.cs
+++ MangaWebApp/Areas/Admin/Controllers/ProductController.cs
@@ -27,7 +27,8 @@ namespace MangaWebApp.Areas.Admin.Controllers
                     Text = u.Name,
                     Value = u.Id.ToString()
                 });
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
             return View();
         }
         [HttpPost]
