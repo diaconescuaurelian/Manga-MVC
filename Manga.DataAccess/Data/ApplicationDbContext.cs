38772fe4d0b1c76239f11d1f88b9038eab1c1f0a
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-05-07 22:27:50 +0300

    Extend IdentityUser

--- Manga.DataAccess/Data/ApplicationDbContext.cs
+++ Manga.DataAccess/Data/ApplicationDbContext.cs
@@ -5,14 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Manga.DataAccess.Data
 {
-    public class ApplicationDbContext :IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
