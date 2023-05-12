a79a6cb67eefe4988696fe23706effc4e9b01ef3
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-05-12 22:18:36 +0300

    Add Order Management functionality

---
+++ MangaWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,157 @@
+﻿using Manga.DataAccess.Repository.IRepository;
+using Manga.Models;
+using Manga.Models.ViewModels;
+using Manga.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace MangaWebApp.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+	{
+
+		private readonly IUnitOfWork _unit;
+        [BindProperty]
+        public OrderViewModel  OrderViewModel { get; set; }
+        public OrderController(IUnitOfWork unit)
+        {
+				_unit = unit;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+        public IActionResult Details(int orderId)
+        {
+            OrderViewModel = new()
+            {
+                OrderHeader = _unit.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetail = _unit.OrderDetail.GetAll(u=>u.OrderId == orderId, includeProperties: "Product")
+            };
+            return View(OrderViewModel);
+        }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult UpdateOrderDetail(int orderId)
+        {
+            var orderHeaderFromDatabase = _unit.OrderHeader.Get(u => u.Id == OrderViewModel.OrderHeader.Id);
+
+            orderHeaderFromDatabase.Name = OrderViewModel.OrderHeader.Name;
+            orderHeaderFromDatabase.PhoneNumber = OrderViewModel.OrderHeader.PhoneNumber;
+            orderHeaderFromDatabase.StreetAddress = OrderViewModel.OrderHeader.StreetAddress;
+            orderHeaderFromDatabase.City = OrderViewModel.OrderHeader.City;
+            orderHeaderFromDatabase.County = OrderViewModel.OrderHeader.County;
+            orderHeaderFromDatabase.PostalCode = OrderViewModel.OrderHeader.PostalCode;
+
+            if (!string.IsNullOrEmpty(OrderViewModel.OrderHeader.Carrier))
+            {
+                orderHeaderFromDatabase.Carrier = OrderViewModel.OrderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderViewModel.OrderHeader.TrackingNumber))
+            {
+                orderHeaderFromDatabase.Carrier = OrderViewModel.OrderHeader.TrackingNumber;
+            }
+            _unit.OrderHeader.Update(orderHeaderFromDatabase);
+            _unit.Save();
+
+            TempData["Success"] = "Order Details Updated Successfully.";
+            return RedirectToAction(nameof(Details), new {orderId = orderHeaderFromDatabase.Id});
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult StartProcessing()
+        {
+            _unit.OrderHeader.UpdateStatus(OrderViewModel.OrderHeader.Id, SD.StatusInProcess);
+            _unit.Save();
+            TempData["Success"] = "Order Details Updated Successfully";
+            return RedirectToAction(nameof(Details), new { orderId = OrderViewModel.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unit.OrderHeader.Get(u => u.Id == OrderViewModel.OrderHeader.Id);
+            orderHeader.TrackingNumber = OrderViewModel.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderViewModel.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate =DateTime.Now;
+            _unit.OrderHeader.Update(orderHeader);
+            _unit.Save();
+            TempData["Success"] = "Order Shipped Successfully";
+            return RedirectToAction(nameof(Details), new { orderId = OrderViewModel.OrderHeader.Id });
+        }
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unit.OrderHeader.Get(u => u.Id == OrderViewModel.OrderHeader.Id);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId
+                };
+
+                var service = new RefundService();
+                Refund refund = service.Create(options);
+                _unit.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+            else
+            {
+                _unit.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+            _unit.Save();
+            TempData["Success"] = "Order Cancelled Successfully";
+            return RedirectToAction(nameof(Details), new { orderId = OrderViewModel.OrderHeader.Id });
+        }
+
+            #region API CALLS
+
+            [HttpGet]
+		public IActionResult GetAll(string status)
+		{
+            IEnumerable<OrderHeader> objOrderHeaders;
+
+            if (User.IsInRole(SD.Role_Admin) || (User.IsInRole(SD.Role_Employee)))
+            {
+                objOrderHeaders = _unit.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unit.OrderHeader.GetAll(u=>u.ApplicationUserId == userId, includeProperties:"ApplicationUser");
+            }
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusPending);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = objOrderHeaders });
+		}
+		#endregion
+	}
+}
